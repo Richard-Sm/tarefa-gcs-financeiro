d07bfefe9a724045cd98d71852652ae887a991bc
root <richard.schmitz@universo.univates.br>
2026-05-14 23:59:48 +0000

    Fix: Permite persistência do campo observacoes no Model e Controller

--- deploy-homolog.sh
+++ deploy-homolog.sh
@@ -5,20 +5,23 @@ echo "=========================================="
 
 # 1. Atualiza o código fonte
 git pull origin main
-
+set -e
 # 2. Define variáveis específicas para Homologação (Porta 8080)
 export COMPOSE_PROJECT_NAME=homolog
 export APP_PORT=8080
 export DB_DATABASE=financas_homolog
+export DB_PORT=3307
+export DB_USERNAME=root      # <-- Força o usuário correto do MySQL
+export DB_PASSWORD=@rec0823!
 export APP_ENV=local
 export APP_DEBUG=true
 
 # 3. Sobe os contêineres e recria se houver mudanças
-docker-compose up -d --build
+docker compose up -d --build
 
 # 4. Aguarda o banco de dados iniciar
 echo "⏳ A aguardar a base de dados..."
-sleep 10
+sleep 40
 
 # 5. Executa as migrações (Versionamento de BD) dentro do contêiner
 docker exec homolog-app-1 php artisan migrate --force
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -10,15 +10,18 @@ git pull origin main
 export COMPOSE_PROJECT_NAME=prod
 export APP_PORT=80
 export DB_DATABASE=financas_prod
+export DB_PORT=3308
+export DB_USERNAME=root      # <-- Força o usuário correto do MySQL
+export DB_PASSWORD=@rec0823!
 export APP_ENV=production
 export APP_DEBUG=false
 
 # 3. Sobe os contêineres e recria se houver mudanças
-docker-compose up -d --build
+docker compose up -d --build
 
 # 4. Aguarda o banco de dados iniciar
 echo "⏳ A aguardar a base de dados..."
-sleep 10
+sleep 40
 
 # 5. Executa as migrações (Versionamento de BD) dentro do contêiner
 docker exec prod-app-1 php artisan migrate --force
