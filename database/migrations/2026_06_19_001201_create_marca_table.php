cee9e7b390fc30bce155335f0b42f7facc061923
root <richard.schmitz@universo.univates.br>
2026-06-19 00:19:27 +0000

    Feat: Cria tabela tipos e altera titulo

--- database/migrations/2026_06_19_001201_create_marca_table.php
+++ database/migrations/2026_06_19_001201_create_marca_table.php
@@ -10,13 +10,14 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('marca', function (Blueprint $table) {
-        $table->id();
-        $table->string('descricao');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('marca', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
     /**
      * Reverse the migrations.
      */
